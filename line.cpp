a2c4aa2637486545cb9e1214273e0865a76e2dbe
MdAkash3135 <mdeaqubmia@gmail.com>
2024-06-22 07:28:45 +0600

    line.cpp is going on progress

---
+++ line.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        vector<ll> v, v1;
+        for (int i = 0; i < n/2; i++)
+        {
+             if(s[i]=='R')
+                v.push_back(n-i-1);
+
+
+        }
+
+
+    }
+
+}
